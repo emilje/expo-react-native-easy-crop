45a26353e31341ce429e2f3858fc4793a951f23b
Emil Jerkovic <emil.jerkovic@gmail.com>
2023-04-07 20:29:31 +0300

    Added tgz to gitignore. Modified installation script for tarball to use correct version.

--- example/run.js
+++ example/run.js
@@ -1,10 +1,13 @@
 const { exec } = require("child_process");
 const { existsSync } = require("fs");
 const { exit } = require("process");
+const {version} = require("../package.json")
 
 const wait = (time) => {
   return new Promise((res) => setTimeout(res, time));
 };
+
+const tarballName =  `expo-react-native-easy-crop-${version}.tgz`
 
 const createTarball = async () => {
   return new Promise((res, rej) => {
@@ -23,12 +26,12 @@ const runScript = async () => {
   let i = 5;
   let tarballLocated = false;
 
-  if (!existsSync("../expo-react-native-easy-crop-1.0.0.tgz")) {
+  if (!existsSync("../" + tarballName)) {
     try {
       console.log("Creating tarball...");
       await createTarball();
       while (i !== 0) {
-        if (existsSync("../expo-react-native-easy-crop-1.0.0.tgz")) {
+        if (existsSync("../" + tarballName)) {
           console.log("Tarball found.");
           tarballLocated = true;
           break;
@@ -44,19 +47,18 @@ const runScript = async () => {
       }
 
       console.log("Installing tarball...");
-      exec("npm run install-tarball", (err) => {
+      exec("npm install ../" + tarballName, (err) => {
         if (err) {
           throw new Error("Something went wrong. Try again.");
         }
+        console.log("Done!");
+        exit();
       });
     } catch (err) {
       console.warn(err.message);
       exit();
     }
   }
-
-  console.log("Done!");
-  exit();
 };
 
 runScript();
