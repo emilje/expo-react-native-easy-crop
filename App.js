0989e84d7571ddbe1a08987fdbc1596a4316a1dd
Emil Jerkovic <emil.jerkovic@gmail.com>
2023-04-08 06:00:07 +0300

    Removed unused code that is now in the example.

--- App.js
+++ App.js
@@ -1,35 +1,5 @@
-import { Text, View } from "react-native";
-import Cropper from "./Cropper";
-
-const ASPECT_RATIO = 10/16;
-const BORDER_RADIUS = 8;
-
 export default function App() {
-  return (
-    <Cropper
-      imageData={imageData}
-      aspectRatio={ASPECT_RATIO}
-      handleCroppedImage={handleCroppedImage}
-      // imageContainerStyle={{ padding: 8 }}
-      // backdropColor={"black"}
-      // borderRadius={BORDER_RADIUS}
-      // header={(handleLayout) => (
-      //   <View
-      //     onLayout={handleLayout}
-      //     style={{
-      //       backgroundColor: "white",
-      //       alignItems: "center",
-      //       padding: 16,
-      //     }}
-      //   >
-      //     <Text>Header Hello!!</Text>
-      //   </View>
-      // )}
-      // footer={(cropImage) => (
-      //   <SafeAreaView style={{ backgroundColor: "green" }}>
-      //     <Button onPress={cropImage} title="Croppi" />
-      //   </SafeAreaView>
-      // )}
-    />
-  );
+  // Nothing to see here, move along :)
+  // (or is there...)
+  return;
 }
